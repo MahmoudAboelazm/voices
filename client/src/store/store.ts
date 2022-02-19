73b28d1b583310f8af314a032087f57d5ea73ec1
MahmoudAboelazm <ma7moudaboelazm@gmail.com>
2022-02-19 22:41:03 +0200

    ✔ Add redux store

---
+++ client/src/store/store.ts
@@ -0,0 +1,28 @@
+import { createWrapper } from "next-redux-wrapper";
+import { applyMiddleware, combineReducers, compose, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import api from "./middlewares/api";
+import { roomApi } from "./reducers/room/apiReducer/reducer";
+import { user } from "./reducers/user/reducer";
+import { shared } from "./reducers/shared/reducer";
+
+const middlewares = [api, thunk];
+const rootReducers = combineReducers({
+  user,
+  roomApi,
+  shared,
+});
+const composeEnhancers =
+  process.env.NODE_ENV !== "production" ? composeWithDevTools({}) : compose;
+const makeStore = () => {
+  return createStore(
+    rootReducers,
+    composeEnhancers(applyMiddleware(...middlewares)),
+  );
+};
+
+const wrapper = createWrapper(makeStore);
+
+export type State = ReturnType<typeof rootReducers>;
+export default wrapper;
